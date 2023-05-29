92483dd7f0d15b6bb01559fc5f87f1d59c88f2cc
AnNH <an.nguyen@heatmob.net>
2023-05-29 13:07:12 +0700

    Enable checking update and config release to github

--- src/ui/components/AboutDialog/index.tsx
+++ src/ui/components/AboutDialog/index.tsx
@@ -1,16 +1,22 @@
 import {
   Box,
+  Button,
+  Field,
   Margins,
+  Throbber,
+  ToggleSwitch,
 } from '@rocket.chat/fuselage';
-import React, { FC } from 'react';
+import { useAutoFocus, useUniqueId } from '@rocket.chat/fuselage-hooks';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
 
 import { RootAction } from '../../../store/actions';
 import { RootState } from '../../../store/rootReducer';
+import { UPDATES_CHECK_FOR_UPDATES_REQUESTED } from '../../../updates/actions';
 import {
-  ABOUT_DIALOG_DISMISSED,
+  ABOUT_DIALOG_DISMISSED, ABOUT_DIALOG_TOGGLE_UPDATE_ON_START,
 } from '../../actions';
 import { Dialog } from '../Dialog';
 import { WorkCECLogo } from '../WorkCECLogo';
@@ -19,95 +25,95 @@ const copyright = `© 2022-${new Date().getFullYear()}, WorkCEC`;
 
 export const AboutDialog: FC = () => {
   const appVersion = useSelector(({ appVersion }: RootState) => appVersion);
-  // const doCheckForUpdatesOnStartup = useSelector(
-  //   ({ doCheckForUpdatesOnStartup }: RootState) => doCheckForUpdatesOnStartup
-  // );
-  // const isCheckingForUpdates = useSelector(
-  //   ({ isCheckingForUpdates }: RootState) => isCheckingForUpdates
-  // );
-  // const isEachUpdatesSettingConfigurable = useSelector(
-  //   ({ isEachUpdatesSettingConfigurable }: RootState) =>
-  //     isEachUpdatesSettingConfigurable
-  // );
-  // const isUpdatingAllowed = useSelector(
-  //   ({ isUpdatingAllowed }: RootState) => isUpdatingAllowed
-  // );
-  // const isUpdatingEnabled = useSelector(
-  //   ({ isUpdatingEnabled }: RootState) => isUpdatingEnabled
-  // );
-  // const newUpdateVersion = useSelector(
-  //   ({ newUpdateVersion }: RootState) => newUpdateVersion
-  // );
+  const doCheckForUpdatesOnStartup = useSelector(
+    ({ doCheckForUpdatesOnStartup }: RootState) => doCheckForUpdatesOnStartup
+  );
+  const isCheckingForUpdates = useSelector(
+    ({ isCheckingForUpdates }: RootState) => isCheckingForUpdates
+  );
+  const isEachUpdatesSettingConfigurable = useSelector(
+    ({ isEachUpdatesSettingConfigurable }: RootState) =>
+      isEachUpdatesSettingConfigurable
+  );
+  const isUpdatingAllowed = useSelector(
+    ({ isUpdatingAllowed }: RootState) => isUpdatingAllowed
+  );
+  const isUpdatingEnabled = useSelector(
+    ({ isUpdatingEnabled }: RootState) => isUpdatingEnabled
+  );
+  const newUpdateVersion = useSelector(
+    ({ newUpdateVersion }: RootState) => newUpdateVersion
+  );
   const openDialog = useSelector(({ openDialog }: RootState) => openDialog);
-  // const updateError = useSelector(({ updateError }: RootState) => updateError);
+  const updateError = useSelector(({ updateError }: RootState) => updateError);
 
   const isVisible = openDialog === 'about';
-  // const canUpdate = isUpdatingAllowed && isUpdatingEnabled;
-  // const isCheckForUpdatesOnStartupChecked =
-  //   isUpdatingAllowed && isUpdatingEnabled && doCheckForUpdatesOnStartup;
-  // const canSetCheckForUpdatesOnStartup =
-  //   isUpdatingAllowed && isEachUpdatesSettingConfigurable;
+  const canUpdate = isUpdatingAllowed && isUpdatingEnabled;
+  const isCheckForUpdatesOnStartupChecked =
+    isUpdatingAllowed && isUpdatingEnabled && doCheckForUpdatesOnStartup;
+  const canSetCheckForUpdatesOnStartup =
+    isUpdatingAllowed && isEachUpdatesSettingConfigurable;
 
   const dispatch = useDispatch<Dispatch<RootAction>>();
 
   const { t } = useTranslation();
 
-  // const [
-  //   [checkingForUpdates, checkingForUpdatesMessage],
-  //   setCheckingForUpdates,
-  // ] = useState([false, null]);
+  const [
+    [checkingForUpdates, checkingForUpdatesMessage],
+    setCheckingForUpdates,
+  ] = useState([false, null]);
 
-  // useEffect(() => {
-  //   if (updateError) {
-  //     setCheckingForUpdates([
-  //       true,
-  //       t('dialog.about.errorWhenLookingForUpdates'),
-  //     ]);
+  useEffect(() => {
+    if (updateError) {
+      setCheckingForUpdates([
+        true,
+        t('dialog.about.errorWhenLookingForUpdates'),
+      ]);
 
-  //     const messageTimer = setTimeout(() => {
-  //       setCheckingForUpdates([false, null]);
-  //     }, 5000);
+      const messageTimer = setTimeout(() => {
+        setCheckingForUpdates([false, null]);
+      }, 5000);
 
-  //     return () => {
-  //       clearTimeout(messageTimer);
-  //     };
-  //   }
+      return () => {
+        clearTimeout(messageTimer);
+      };
+    }
 
-  //   if (isCheckingForUpdates) {
-  //     setCheckingForUpdates([true, null]);
-  //     return undefined;
-  //   }
+    if (isCheckingForUpdates) {
+      setCheckingForUpdates([true, null]);
+      return undefined;
+    }
 
-  //   if (newUpdateVersion) {
-  //     setCheckingForUpdates([false, null]);
-  //     return undefined;
-  //   }
+    if (newUpdateVersion) {
+      setCheckingForUpdates([false, null]);
+      return undefined;
+    }
 
-  //   setCheckingForUpdates([true, t('dialog.about.noUpdatesAvailable')]);
-  //   const messageTimer = setTimeout(() => {
-  //     setCheckingForUpdates([false, null]);
-  //   }, 5000);
+    setCheckingForUpdates([true, t('dialog.about.noUpdatesAvailable')]);
+    const messageTimer = setTimeout(() => {
+      setCheckingForUpdates([false, null]);
+    }, 5000);
 
-  //   return () => {
-  //     clearTimeout(messageTimer);
-  //   };
-  // }, [updateError, isCheckingForUpdates, newUpdateVersion, t]);
+    return () => {
+      clearTimeout(messageTimer);
+    };
+  }, [updateError, isCheckingForUpdates, newUpdateVersion, t]);
 
-  // const handleCheckForUpdatesButtonClick = (): void => {
-  //   dispatch({ type: UPDATES_CHECK_FOR_UPDATES_REQUESTED });
-  // };
+  const handleCheckForUpdatesButtonClick = (): void => {
+    dispatch({ type: UPDATES_CHECK_FOR_UPDATES_REQUESTED });
+  };
 
-  // const handleCheckForUpdatesOnStartCheckBoxChange = (
-  //   event: ChangeEvent<HTMLInputElement>
-  // ): void => {
-  //   dispatch({
-  //     type: ABOUT_DIALOG_TOGGLE_UPDATE_ON_START,
-  //     payload: event.target.checked,
-  //   });
-  // };
+  const handleCheckForUpdatesOnStartCheckBoxChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    dispatch({
+      type: ABOUT_DIALOG_TOGGLE_UPDATE_ON_START,
+      payload: event.target.checked,
+    });
+  };
 
-  // const checkForUpdatesButtonRef = useAutoFocus(isVisible);
-  // const checkForUpdatesOnStartupToggleSwitchId = useUniqueId();
+  const checkForUpdatesButtonRef = useAutoFocus(isVisible);
+  const checkForUpdatesOnStartupToggleSwitchId = useUniqueId();
 
   return (
     <Dialog
@@ -126,7 +132,7 @@ export const AboutDialog: FC = () => {
           </Trans>
         </Box>
 
-        {/* {canUpdate && (
+        {canUpdate && (
           <Box display='flex' flexDirection='column'>
             <Margins block='x8'>
               {!checkingForUpdates && (
@@ -170,7 +176,7 @@ export const AboutDialog: FC = () => {
               </Field.Row>
             </Margins>
           </Box>
-        )} */}
+        )}
 
         <Box alignSelf='center' fontScale='micro'>
           {t('dialog.about.copyright', { copyright })}
